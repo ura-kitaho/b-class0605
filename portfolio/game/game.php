9bfdd5b35e012d0a27de43739c12190b3daceb13
USENGROUP\sh-kubota <sh-kubota@usen-next.jp>
2023-06-07 16:13:14 +0900

    nav追加

--- portfolio/game/game.php
+++ portfolio/game/game.php
@@ -1,72 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../common/style.css">
-    <title>じゃんけんゲーム</title> 
+    <title>じゃんけんゲーム</title>
 </head>
+
 <body>
 
-<p>ここからいろんなところに飛べるよ！！！</p>
-    <ul>
-            <li><a href="http://localhost/b-class0605/portfolio/form/form.html">form</a></li>
-            <li><a href="http://localhost/b-class0605/portfolio/gacha/gacha.php">gacha</a></li>
-            <li><a href="http://localhost/b-class0605/portfolio/game/game.php">game</a></li>
-            <li><a href="http://localhost/b-class0605/portfolio/calc/calc.php">calc</a></li>
-            <li><a href="http://localhost/b-class0605/portfolio/index.html">index</a></li>
-        </ul>
+    <h2>ここからいろんなところに飛べるよ！！！</h2>
+    <h3>下の気になるとこから好きなところ行っちゃって～</h3>
+    <ul class=nav>
+        <li><a href="http://localhost/b-class0605/portfolio/form/form.html">form</a></li>
+        <li><a href="http://localhost/b-class0605/portfolio/gacha/gacha.php">gacha</a></li>
+        <li><a href="http://localhost/b-class0605/portfolio/game/game.php">game</a></li>
+        <li><a href="http://localhost/b-class0605/portfolio/calc/calc.php">calc</a></li>
+        <li><a href="http://localhost/b-class0605/portfolio/index.html">index</a></li>
+    </ul>
 
     <form action="game.php">
         <input type="radio" name="janken" value="グー">グー<br>
         <input type="radio" name="janken" value="チョキ">チョキ<br>
         <input type="radio" name="janken" value="パー">パー<br>
         <input type="submit" name="submit" value="じゃんけん！">
-</form>
+    </form>
 
-<?php
-if(isset($_GET["janken"])){
-    $jibun = $_GET["janken"];
+    <?php
+    if (isset($_GET["janken"])) {
+        $jibun = $_GET["janken"];
 
-    $aitenote = ["グー", "チョキ", "パー"];
-    $dasute = rand(0,2);
-    $aite = $aitenote[$dasute];
+        $aitenote = ["グー", "チョキ", "パー"];
+        $dasute = rand(0, 2);
+        $aite = $aitenote[$dasute];
 
-    echo "自分:{$jibun}" . "<br>";
-    echo "相手:{$aite}". "<br>";
+        echo "自分:{$jibun}" . "<br>";
+        echo "相手:{$aite}" . "<br>";
 
-    if($jibun == $aite){
-        echo "あいこ";
-    }
-
-    if($jibun == "グー"){
-        if($aite == "チョキ"){
-            echo "あなたの勝ち";
-        }elseif($aite == "パー"){
-            echo "あなたの負け";
+        if ($jibun == $aite) {
+            echo "あいこ";
         }
+
+        if ($jibun == "グー") {
+            if ($aite == "チョキ") {
+                echo "あなたの勝ち";
+            } elseif ($aite == "パー") {
+                echo "あなたの負け";
+            }
+        }
+
+        if ($jibun == "チョキ") {
+            if ($aite == "パー") {
+                echo "あなたの勝ち";
+            } elseif ($aite == "グー") {
+                echo "あなたの負け";
+            }
+        }
+
+        if ($jibun == "パー") {
+            if ($aite == "グー") {
+                echo "あなたの勝ち";
+            } elseif ($aite == "チョキ") {
+                echo "あなたの負け";
+            }
+        }
+    } else {
+        echo "出す手を選んでね！";
     }
 
-    if($jibun == "チョキ"){
-        if($aite == "パー"){
-            echo "あなたの勝ち";
-        }elseif($aite == "グー"){
-            echo "あなたの負け";
-        }
-    }
-
-    if($jibun == "パー"){
-        if($aite == "グー"){
-            echo "あなたの勝ち";
-        }elseif($aite == "チョキ"){
-            echo "あなたの負け";
-        }
-    }
-}else{
-    echo "出す手を選んでね！";
-}
-        
-?>
+    ?>
 </body>
+
 </html>
